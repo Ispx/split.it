9e29a3b2892eec8d0fb31b4eb75f08e748b62356
Isaque Paixão <isaquespx98@gmail.com>
2021-06-15 22:10:28 -0300

    Created AppWidget

---
+++ lib/core/widgets/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:split_it/core/theme/theme_app.dart';
+import 'package:split_it/modules/splash/pages/splash_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemUiOverlayStyle(statusBarColor: ThemeApp.config.primaryColor);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
+}
