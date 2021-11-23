bb16e68b4a42ae410985296a9ca7be66140b21e0
Isaque Paixão <isaquespx98@gmail.com>
2021-11-22 23:21:27 -0300

    created model

---
+++ lib/modules/steps/models/personal_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+class PersonalModel {
+  final String? firstName;
+  final String? secondName;
+  final String? urlImage;
+  PersonalModel({
+    @required this.firstName,
+    @required this.secondName,
+    @required this.urlImage,
+  });
+}
