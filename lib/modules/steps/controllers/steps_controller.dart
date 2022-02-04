0015c3d02ae985815d938864def5abf7da49e527
Isaque Paixao <isaquespx98@gmail.com>
2022-02-03 23:31:18 -0300

    implemented seachFriend

--- lib/modules/steps/controllers/steps_controller.dart
+++ lib/modules/steps/controllers/steps_controller.dart
@@ -1,17 +1,23 @@
 import 'package:mobx/mobx.dart';
+import 'package:split_it/core/shared/firebase_repository.dart';
 import 'package:split_it/modules/steps/models/personal_model.dart';
 part 'steps_controller.g.dart';
 
 class StepsController = _StepsControllerBase with _$StepsController;
 
 abstract class _StepsControllerBase with Store {
+  FirebaseRepository? _repository;
   @observable
   String? _title;
   @observable
   int _currentStep = 0;
   @observable
   int _stepsLength;
+  @observable
+  String _seach = '';
+
   _StepsControllerBase(this._stepsLength);
+
   @observable
   PersonalModel? _personalModel;
   String get title => _title ?? "";
@@ -19,6 +25,24 @@ abstract class _StepsControllerBase with Store {
   PersonalModel get personalModel =>
       _personalModel ??
       PersonalModel(firstName: '', secondName: '', urlImage: '');
+  @computed
+  Future<List<Map<String, dynamic>>> get seachFriend async {
+    _repository = FirebaseRepository('/friends/');
+    try {
+      if (this._seach.isNotEmpty) {
+        return await _repository!.where(field: 'name', value: _seach);
+      }
+      return [];
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @action
+  void changeSearch(String seach) {
+    this._seach = seach;
+  }
+
   @action
   void nextStep() {
     if (_currentStep == _stepsLength - 1) return;
@@ -30,7 +54,7 @@ abstract class _StepsControllerBase with Store {
     if (_currentStep == 0) return;
     _currentStep--;
   }
-  
+
   @action
   void changePersonal(PersonalModel personalModel) {
     this._personalModel = personalModel;
