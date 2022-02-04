978e00b3cc706a5f7e5ea458650297e09f3ca8b8
Isaque Paixao <isaquespx98@gmail.com>
2022-02-03 23:31:29 -0300

    created FirebaseRepository

---
+++ lib/core/shared/firebase_repository.dart
@@ -0,0 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:split_it/core/models/base_model.dart';
+
+class FirebaseRepository extends BaseModel {
+  late FirebaseFirestore _firebaseFirestore;
+  FirebaseRepository(String collection) : super(collection) {
+    this._firebaseFirestore = FirebaseFirestore.instance;
+  }
+  Future create() async {
+    try {
+      final response =
+          await _firebaseFirestore.collection(collenction).add(toMap!);
+      return response.id;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> read({required collection}) async {
+    try {
+      final response = await _firebaseFirestore.collection(collection).get();
+      return response.docs.map((e) => e.data()).toList();
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> where(
+      {required field, required value}) async {
+    try {
+      final response = await _firebaseFirestore
+          .collection(collenction)
+          .where(field, isEqualTo: value)
+          .get();
+      return response.docs.map((e) => e.data()).toList();
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<Map<String, dynamic>> firstWhere(
+      {required doc, required field, required value}) async {
+    try {
+      final response = await _firebaseFirestore
+          .collection(collenction)
+          .where(field, isEqualTo: value)
+          .get();
+      return response.docs.first.data();
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future delete() async {}
+
+  Future updae() {
+    // TODO: implement updae
+    throw UnimplementedError();
+  }
+}
