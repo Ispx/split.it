82f83d9569baa056d32434ae55d15a57e4d0d0f6
Isaque Paixao <isaquespx98@gmail.com>
2022-02-07 23:14:53 -0300

    created factory fromJson

--- lib/modules/steps/models/personal_model.dart
+++ lib/modules/steps/models/personal_model.dart
@@ -5,10 +5,15 @@ class PersonalModel {
   final String? secondName;
   final String? urlImage;
   bool? isSelected = false;
-  
+
   PersonalModel({
     @required this.firstName,
     @required this.secondName,
     @required this.urlImage,
   });
+  factory PersonalModel.fromJson(Map map) => PersonalModel(
+        firstName: map['first_name'],
+        secondName: map['second_name'],
+        urlImage: map['url_image'],
+      );
 }
