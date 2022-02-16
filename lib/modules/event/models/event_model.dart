350dd85f704ee08d50619a98fdaade0aa32755b4
Isaque Paixao <isaquespx98@gmail.com>
2022-02-16 09:16:18 -0300

    add atributes in EventModel

--- lib/modules/event/models/event_model.dart
+++ lib/modules/event/models/event_model.dart
@@ -1,17 +1,47 @@
-import 'package:flutter/foundation.dart';
-import 'package:split_it/modules/home/components/indicator_operation_balance.dart';
+import 'dart:convert';
+
+import 'package:split_it/modules/steps/models/item_model.dart';
+import 'package:split_it/modules/steps/models/personal_model.dart';
 
 class EventModel {
   final String? title;
   final String? imagePath;
   final DateTime? createdAt;
-  final int? people;
+  final List<ItemModel>? items;
+  final List<PersonalModel>? friends;
   final double? totalAmount;
-  EventModel({
-    @required this.title,
-    @required this.imagePath,
-    @required this.createdAt,
-    @required this.totalAmount,
-    @required this.people,
-  });
+
+  int get people => friends?.length ?? 0;
+  EventModel(
+      {this.title,
+      this.imagePath,
+      this.createdAt,
+      this.totalAmount,
+      this.friends,
+      this.items});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'imagePath': imagePath,
+      'createdAt': createdAt,
+      'items': items,
+      'friends': friends,
+      'totalAmount': totalAmount,
+    };
+  }
+
+  factory EventModel.fromMap(Map<String, dynamic> map) {
+    return EventModel(
+      title: map['title'],
+      imagePath: map['imagePath'],
+      createdAt: map['createdAt'],
+      items: map['items']?.map((x) => ItemModel?.fromMap(x)),
+      friends: map['friends']?.map((x) => PersonalModel?.fromMap(x)),
+      totalAmount: map['totalAmount'],
+    );
+  }
+
+  factory EventModel.fromJson(String source) =>
+      EventModel.fromMap(json.decode(source));
 }
