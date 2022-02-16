cd4061601fa33447edf6891e8fae05dbfd85fad4
Isaque Paixao <isaquespx98@gmail.com>
2022-02-16 09:17:15 -0300

    created toMap in ItemModel

--- lib/modules/steps/models/item_model.dart
+++ lib/modules/steps/models/item_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ItemModel {
   final String? name;
   final double? amount;
@@ -14,4 +16,23 @@ class ItemModel {
       amount: amount ?? this.amount,
     );
   }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'amount': amount,
+    };
+  }
+
+  factory ItemModel.fromMap(Map<String, dynamic> map) {
+    return ItemModel(
+      name: map['name'],
+      amount: map['amount'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ItemModel.fromJson(String source) =>
+      ItemModel.fromMap(json.decode(source));
 }
