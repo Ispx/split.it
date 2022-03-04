4a3bfe34ddf971b74966de4e0a34264b105b450a
Isaque Paixao <isaquespx98@gmail.com>
2022-03-04 08:47:29 -0300

    fix

--- lib/core/models/balance_model.dart
+++ lib/core/models/balance_model.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/foundation.dart';
-import 'package:split_it/modules/home/components/indicator_operation_balance.dart';
-
 class BalanceModel {
   final double? amountRecived;
   final double? amountToPay;
   BalanceModel({
-    @required this.amountRecived,
-    @required this.amountToPay,
+    required this.amountRecived,
+    required this.amountToPay,
   });
 }
